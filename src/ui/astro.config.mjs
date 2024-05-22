c4a8eb01a7eed011655829820d86f01c23e83bf8
withuser <rafaelramos.soporte@gmail.com>
2024-05-22 12:02:09 -0500

    chore: node adapter

--- src/ui/astro.config.mjs
+++ src/ui/astro.config.mjs
@@ -1,13 +1,18 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   // TODO:
-  site: "https://example.com",
+  site: 'https://example.com',
   integrations: [
     react(),
-    tailwind({ configFile: "./src/ui/tailwind.config.mjs" }),
+    tailwind({ configFile: './src/ui/tailwind.config.mjs' }),
   ],
+  adapter: node({
+    mode: 'standalone',
+  }),
+  output: 'server',
 });
